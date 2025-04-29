c460d889ad592a1fd126d9a433b4208d04045c66
iamwicked <googol8081@gmail.com>
2025-04-29 13:47:43 +0530

    user authentication added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
  
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const authMiddleware = require('./middleware/auth');
+const cors = require('cors');
+
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // or your frontend URL
+    credentials: true
+  }));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({ message: 'Protected route accessed' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
